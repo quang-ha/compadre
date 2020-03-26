06b55b25d3aa3ed95644407ce80a9153b6367f72
Quang Ha <qth20@bu.edu>
2020-03-26 10:59:07 -0400

    Fix to neighbor index - now working for serial

--- physics/Compadre_GMLS_Stokes_Sources.cpp
+++ physics/Compadre_GMLS_Stokes_Sources.cpp
@@ -96,7 +96,10 @@ void GMLS_StokesSources::evaluateRHS(local_index_type field_one, local_index_typ
                             collapse_value += normal_comp*_physics->_velocity_all_GMLS->getAlpha1TensorTo1Tensor(TargetOperation::CurlCurlOfVectorPointEvaluation, boundary_filtered_flags(i), m /* output component */, l, n /* input component */);
                         }
                         // Get neighbor pt
-                        xyz_type neighbor_pt(pts(l, 0), pts(l, 1), pts(l, 2));
+                        local_index_type neighbor_idx = neighborhood->getNeighbor(boundary_filtered_flags(i), l);
+                        xyz_type neighbor_pt(pts(neighbor_idx, 0),
+                                             pts(neighbor_idx, 1),
+                                             pts(neighbor_idx, 2));
                         curlcurl_fdotn_neighbor_value += collapse_value*velocity_true_function->evalVector(neighbor_pt)[n];
                     }
                     curlcurl_fdotn_target_value += curlcurl_fdotn_neighbor_value;
