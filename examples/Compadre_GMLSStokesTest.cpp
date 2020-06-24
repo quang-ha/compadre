34b55d0f34a0c9ed6fe1d5a55a39c4d4627c7028
Quang Ha <qth20@bu.edu>
2020-06-24 01:10:55 -0400

    Fix output file naming convention

--- examples/Compadre_GMLSStokesTest.cpp
+++ examples/Compadre_GMLSStokesTest.cpp
@@ -61,8 +61,8 @@ int main(int argc, char* args[]) {
         std::vector<double> velocity_errors(1);
         std::vector<double> pressure_errors(1);
         const std::string filename_prefix = parameters->get<Teuchos::ParameterList>("io").get<std::string>("input file prefix");
-        const std::string size = "0.2.nc";
-        hsize[0] = 0.2;
+        const std::string size = "0.4.nc";
+        hsize[0] = 0.4;
         fnames[0] = filename_prefix + size;
 
         TEUCHOS_TEST_FOR_EXCEPT_MSG(parameters->get<int>("loop size")>3, "Only three mesh levels available for this problem.");
@@ -247,7 +247,7 @@ int main(int argc, char* args[]) {
             pressure_errors[i] = error_pressure_global_norm;
 
             WriteTime->start();
-            std::string output_filename = parameters->get<Teuchos::ParameterList>("io").get<std::string>("output file prefix") + parameters->get<Teuchos::ParameterList>("io").get<std::string>("output file") + size;
+            std::string output_filename = parameters->get<Teuchos::ParameterList>("io").get<std::string>("output file prefix") + size;
             fm.setWriter(output_filename, particles);
             fm.write();
             WriteTime->stop();
